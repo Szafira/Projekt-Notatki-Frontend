e04e931697f47505cea3f47e0726bb68aab32566
Klaudia Sawicka <sawicka.klaudi@gmail.com>
2021-06-12 00:41:54 +0200

    Text-editor at work

---
+++ src/app/text-editor/text-editor.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-editor',
+  template: `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <script src="https://cdn.tiny.cloud/1/mm3bfz08zx0oabulhh1lo1hy7yc3hmdlanbxoaxtgd1g7cpp/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+  </head>
+  <body>
+    <textarea>
+      Welcome to TinyMCE!
+    </textarea>
+    <script>
+      tinymce.init({
+        selector: 'textarea',
+        plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
+        toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
+        toolbar_mode: 'floating',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'Author name'
+      });
+    </script>
+  </body>
+  </html>
+  `
+  ,
+  styles: []
+})
+export class TextEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
